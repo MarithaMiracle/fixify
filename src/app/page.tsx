71bcb313f68fe6341736780aa5d584687547ad40
Maritha Miracle <marithamiracle@gmail.com>
2025-07-25 19:48:18 +0100

    Frontend integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,27 @@
 "use client";
 
 import React from 'react';
+import Link from 'next/link';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import CallToActionProvider from './components/CallToActionProvider';
 import HeroSection from './components/HeroSection';
 import FeaturedCategories from './components/FeaturedCategories';
 import HowItWorks from './components/HowItWorks';
-import TestimonialsSlider from './components/TestimonialsSlider';
 import WhyChooseFixify from './components/WhyChooseFixify';
+import TestimonialsSlider from './components/TestimonialsSlider';
+import CallToActionProvider from './components/CallToActionProvider';
 
-export default function App() {
+
+export default function HomePage() {
     return (
-            <div className="min-h-screen flex flex-col">
-                <Navbar />
-                <main className="flex-grow">
-                    <HeroSection />
-                    <FeaturedCategories />
-                    <HowItWorks />
-                    <WhyChooseFixify />
-                    <TestimonialsSlider />
-                    <CallToActionProvider />
-                </main>
-                <Footer />
-            </div>
-      
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-grow">
+                <HeroSection />
+                <FeaturedCategories />
+                <HowItWorks />
+                <WhyChooseFixify />
+                <TestimonialsSlider />
+                <CallToActionProvider />
+            </main>
+        </div>
     );
 }
